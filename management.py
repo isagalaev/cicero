19693ac97d4c4b5e8d8a060c1fb9f3ec5c2f1451
maniac <>
2007-03-05 15:53:23 +0000

    Проверка на существование cicero_guest

--- management.py
+++ management.py
@@ -2,12 +2,15 @@
 
 from django.dispatch import dispatcher
 from django.db.models import signals
-import cicero.models
 
 def init(signal, sender, app, created_models):
+  import cicero.models
   if app == cicero.models:
     from django.contrib.auth.models import User
-    password = User.objects.make_random_password()
-    User.objects.create_user('cicero_guest', 'cicero_guest@localhost', password)
+    try:
+      cicero_guest = User.objects.get(username='cicero_guest')
+    except User.DoesNotExist:
+      password = User.objects.make_random_password()
+      User.objects.create_user('cicero_guest', 'cicero_guest@localhost', password)
 
 dispatcher.connect(init, signal=signals.post_syncdb)
