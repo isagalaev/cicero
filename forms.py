e065f55331e8d9bff25ee310d11841108063fe2a
maniac <>
2007-03-08 19:15:31 +0000

    Рефакторинг обработки создания статей из views в forms

--- forms.py
+++ forms.py
@@ -2,11 +2,39 @@
 
 from django.newforms import *
 
+def _create_article(topic, user, data):
+  if not user.is_authenticated():
+    from django.contrib.auth.models import User
+    user = User.objects.get(username='cicero_guest')
+  topic.article_set.create(
+    text=data['text'], 
+    author=user,
+    guest_name=data['name'],
+    filter=user.cicero_profile.filter,
+  )
+
 class ArticleForm(Form):
   text = CharField(label='Текст', required=True, widget=Textarea(attrs={'cols': '80', 'rows': '20'}))
   name = CharField(label='Имя', required=True)
+  
+  def __init__(self, topic, user, *args, **kwargs):
+    super(ArticleForm, self).__init__(*args, **kwargs)
+    self.topic, self.user = topic, user
+  
+  def save(self):
+    _create_article(self.topic, self.user, self.clean_data)
   
 class TopicForm(Form):
   subject = CharField(label='Тема', required=True)
   text = CharField(label='Текст', required=True, widget=Textarea(attrs={'cols': '80', 'rows': '20'}))
   name = CharField(label='Имя', required=True)
+  
+  def __init__(self, forum, user, *args, **kwargs):
+    super(TopicForm, self).__init__(*args, **kwargs)
+    self.forum, self.user = forum, user
+  
+  def save(self):
+    from cicero.models import Topic
+    topic = Topic(forum=self.forum, subject=self.clean_data['subject'])
+    topic.save()
+    _create_article(topic, self.user, self.clean_data)
--- views.py
+++ views.py
@@ -8,41 +8,28 @@ from django.template import RequestContext
 from cicero.models import Forum, Topic
 from cicero.forms import ArticleForm, TopicForm
 
-def _create_article(topic, form, user):
-  if not user.is_authenticated():
-    from django.contrib.auth.models import User
-    user = User.objects.get(username='cicero_guest')
-  topic.article_set.create(
-    text=form.clean_data['text'], 
-    author=user,
-    guest_name=form.clean_data['name'],
-    filter=user.cicero_profile.filter,
-  )
-
 def forum(request, slug, **kwargs):
   forum = get_object_or_404(Forum, slug=slug)
   if request.method == 'POST':
-    form = TopicForm(request.POST)
+    form = TopicForm(forum, request.user, request.POST)
     if form.is_valid():
-      topic = Topic(forum=forum, subject=form.clean_data['subject'])
-      topic.save()
-      _create_article(topic, form, request.user)
+      form.save()
       return HttpResponseRedirect('./')
   else:
-    form = TopicForm()
+    form = TopicForm(forum, request.user)
   kwargs['queryset'] = forum.topic_set.all()
   kwargs['extra_context'] = {'forum': forum, 'form': form}
   return object_list(request, **kwargs)
 
 def topic(request, slug, id, **kwargs):
-  obj = get_object_or_404(Topic, forum__slug=slug, pk=id)
+  topic = get_object_or_404(Topic, forum__slug=slug, pk=id)
   if request.method == 'POST':
-    form = ArticleForm(request.POST)
+    form = ArticleForm(topic, request.user, request.POST)
     if form.is_valid():
-      _create_article(obj, form, request.user)
+      form.save()
       return HttpResponseRedirect('./')
   else:
-    form = ArticleForm()
-  kwargs['queryset'] = obj.article_set.all()
-  kwargs['extra_context'] = {'topic': obj, 'form': form}
-  return object_list(request, **kwargs)  
+    form = ArticleForm(topic, request.user)
+  kwargs['queryset'] = topic.article_set.all()
+  kwargs['extra_context'] = {'topic': topic, 'form': form}
+  return object_list(request, **kwargs)
