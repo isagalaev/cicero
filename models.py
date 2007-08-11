ed2ec8bb99d1fec28fffa0c680901ea1933a9f89
maniac <>
2007-08-11 15:00:39 +0000

    str для ключей memcached

--- models.py
+++ models.py
@@ -69,7 +69,7 @@ class ArticleManager(models.Manager):
     return ArticleQuerySet(self.model).filter(deleted__isnull=True)
     
   def latest(self, request, slug, id=None, *args, **kwargs):
-    key = 'latest-article-%s-%s' % (slug, id)
+    key = 'latest-article-%s-%s' % (str(slug), id)
     value = cache.get(key)
     if value is None:
       queryset = self.model.objects.filter(topic__forum__slug=slug).order_by('-created')
@@ -79,9 +79,9 @@ class ArticleManager(models.Manager):
       cache.set(key, value)
     return value
   
-  def invalidate_cache(self, slug, topic_id):
-    cache.delete('latest-article-%s-%s' % (slug, None))
-    cache.delete('latest-article-%s-%s' % (slug, topic_id))
+  def invalidate_cache(self, slug, topic_id)
+    cache.delete('latest-article-%s-%s' % (str(slug), None))
+    cache.delete('latest-article-%s-%s' % (str(slug), topic_id))
 
 class DeletedArticleManager(models.Manager):
   def get_query_set(self):
