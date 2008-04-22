c8d2bc172d9b805643207823c792e57a9804f8cf
maniac <>
2008-04-22 20:24:15 +0000

    quiet-режим для update_whitelist

--- management/commands/update_whitelist.py
+++ management/commands/update_whitelist.py
@@ -1,10 +1,16 @@
 # -*- coding:utf-8 -*-
+from optparse import make_option
+
 from django.core.management.base import NoArgsCommand
 
 class Command(NoArgsCommand):
+  option_list = NoArgsCommand.option_list + (
+    make_option('--quiet', action='store_true', dest='quiet', default=False,
+      help=u'Не выводить сообщения, кроме ошибок'),
+  )
   help = u'Обновляет "белые списки" OpenID из внешних источников'
     
-  def handle_noargs(self, **base_options):
+  def handle_noargs(self, quiet=False, **base_options):
     from cicero.models import CleanOpenID, WhitelistSource
     from urllib2 import urlopen, Request
     for source in WhitelistSource.objects.all():
@@ -12,6 +18,7 @@ class Command(NoArgsCommand):
         f = urlopen(Request(source.url, headers={'Accept': 'text/plain'}))
         source.cleanopenid_set.all().delete()
         source.cleanopenid_set = [CleanOpenID(openid=l.strip()) for l in f]
-        print 'Updated %s OpenIDs from %s' % (source.cleanopenid_set.count(), source)
+        if not quiet:
+          print 'Updated %s OpenIDs from %s' % (source.cleanopenid_set.count(), source)
       except IOError, e:
         print 'Error reading %s: %s' % (source, e)
