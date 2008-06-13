1be1e53bdd4b88977de571996c03cd9a58d69e8e
maniac <>
2008-06-13 10:26:27 +0000

    Отказ от излишней гибкости с CICERO_TEMPLATE_BASE

--- context.py
+++ context.py
@@ -3,6 +3,5 @@
 def default(request):
   from django.conf import settings
   return {
-    'template_base': settings.CICERO_TEMPLATE_BASE,
     'profile': request.user.is_authenticated() and request.user.cicero_profile,
   }
