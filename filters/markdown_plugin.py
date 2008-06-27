8f25aaa1e06dee922f7ac6c9fc83b3bedd65340d
maniac <>
2008-06-27 06:55:49 +0000

    Использование markdown2 перед markdown в фильтре

--- filters/markdown_plugin.py
+++ filters/markdown_plugin.py
@@ -1,8 +1,15 @@
 # -*- coding:utf-8 -*-
 
-def to_html(value):
+try
+  from markdown2 import markdown
+  
+  def to_html(value):
+    return markdown(value, safe_mode='escape')
+except ImportEror:
   from markdown import markdown
-  return markdown(value, safe_mode=True)
-    
+  
+  def to_html(value):
+    return markdown(value, safe_mode=True)
+
 def name():
   return 'markdown'
