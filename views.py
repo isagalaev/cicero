89f3fff5e2513b91604635c772de933449fd9886
Ivan Sagalaev <isagalaev@yandex-team.ru>
2009-12-25 19:53:14 +0300

    Ответ 401 из login_required при ajax-запросе

--- views.py
+++ views.py
@@ -42,7 +42,10 @@ def post_redirect(request):
 def login_required(func):
     def wrapper(request, *args, **kwargs):
         if not request.user.is_authenticated():
-            return HttpResponseRedirect(reverse('scipio_login') + '?redirect=' + request.path)
+            if request.is_ajax():
+                return HttpResponse('Authorization required', status=401, mimetype='text/plain')
+            else:
+                return HttpResponseRedirect(reverse('scipio_login') + '?redirect=' + request.path)
         return func(request, *args, **kwargs)
     return wrapper
 
