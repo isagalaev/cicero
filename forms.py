7049d7344e4586d095be99770e547e430635c920
Ivan Sagalaev <maniac@maniac-laptop>
2010-01-09 14:35:54 +0300

    Не показывать уже отщеплённые статьи в списке на отщепление.

--- forms.py
+++ forms.py
@@ -109,7 +109,9 @@ class SpawnForm(Form):
     def __init__(self, article, *args, **kwargs):
         super(SpawnForm, self).__init__(*args, **kwargs)
         self.article = article
-        articles = article.topic.article_set.filter(pk__gt=article.id).select_related('author', 'author__user')
+        articles = article.topic.article_set
+        articles = articles.filter(pk__gt=article.id, spawned_to=None)\
+                           .select_related('author', 'author__user')
         self.fields['articles'].choices = [(a.id, a) for a in articles]
 
     def save(self):
